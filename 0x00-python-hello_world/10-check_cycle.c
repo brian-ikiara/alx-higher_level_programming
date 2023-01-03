893a7a07d92dd68011f3780bd93a59f8a83fe0f3
brian-ikiara <brnikiara@gmail.com>
2023-01-03 11:03:16 +0000

    Create 10-check_cycle.c, lists.h

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * check_cycle - Function
+ * @list: Head
+ *
+ * Description: Checks for a loop/cycle in a linked
+ * list.
+ * Return: 1, if there's a loop; 0, if none.
+ * On error, stderr.
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *init = list, *final = list;
+
+	do {
+		init = init->next, final = final->next->next;
+		if (init == final)
+			return (1);
+	} while (init && final && final->next);
+
+	return (0);
+}
