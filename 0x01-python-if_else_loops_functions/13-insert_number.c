5676b0dcb5697aaeb9d299f98aba7da12712cd62
brian-ikiara <brnikiara@gmail.com>
2023-01-04 19:12:56 +0000

    Update 13-insert_number.c

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -15,36 +15,40 @@ listint_t *insert_node(listint_t **head, int number)
 {
 	listint_t *temp = NULL, *init = NULL, *added = NULL;
 
-	/* Edge case: head is NULL */
 	if (head == NULL)
 		return (NULL);
-	/* Assign temp the address of head */
 	temp = *head;
-	/* Allocate memory to added */
 	added = malloc(sizeof(listint_t));
-	/* Edge case: added is NULL */
 	if (added == NULL)
 		return (NULL);
-	/* Assign number to member n and NULL to member next */
-	added->n = number, added->next = NULL;
-	/* Create the new node */
+	added->n = number;
+	added->next = NULL;
 	while (temp != NULL)
 	{
 		if (temp->n > number)
 		{
 			if (init != NULL)
-				added->next = temp->next, init->next = added;
+			{
+				added->next = temp->next;
+				init->next = added;
+			}
 			else
-				added->next = temp, *head = added;
+			{
+				added->next = temp;
+				*head = added;
+			}
 			return (added);
 		}
-		init = temp, temp = temp->next;
+		init = temp;
+		temp = temp->next;
 	}
-	/* Edge case: dereferenced head is NULL */
 	if (*head == NULL)
 		*head = added;
 	else
-		added->next = init->next, init->next = added;
+	{
+		added->next = init->next;
+		init->next = added;
+	}
 
 	return (added);
 }
