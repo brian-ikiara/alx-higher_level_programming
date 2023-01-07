acd2eb51c78471085381f75305a64ab948336718
brian-ikiara <brnikiara@gmail.com>
2023-01-07 19:52:20 +0000

    Create 5-variable_load.py

---
+++ 0x02-python-import_modules/5-variable_load.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+    Begin:
+        Import variable_load_5 as varLoad
+        Print varLoad.a using str.format()
+    End
+"""
+import variable_load_5 as varLoad
+
+if __name__ == "__main__":
+    print("{}".format(varLoad.a))
