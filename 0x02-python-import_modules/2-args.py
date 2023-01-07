b205bb13628d7a03f13802e5fd707d29e6c1cf1a
brian-ikiara <brnikiara@gmail.com>
2023-01-07 13:02:15 +0000

    Update 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,5 @@
 #!/usr/bin/python3
-# Import the sys module
 from sys import argv
-"""
-    Initialize l as length of sys.argv
-    Check if length of sys.argv is not zero
-    then print the length of sys.argv then
-    print the cmd args; otherwise, print
-    zero
-"""
 i = 1
 length = len(argv)
 if length != 1:
