af8a7f7cb452c5c093661092c95420a5dc930785
brian-ikiara <brnikiara@gmail.com>
2023-01-15 10:02:31 +0000

    Create 12-roman_to_int.py

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+def subza(idx):
+    res, max_idx = 0, max(idx)
+
+    for i in idx:
+        if max_idx > i:
+            res += i
+    return (max_idx - res)
+
+
+def roman_to_int(roman_string):
+    if not roman_string:
+        return 0
+    if not isinstance(roman_string, str):
+        return 0
+    romans = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    keys = list(romans)
+
+    num, last, idx = 0, 0, [0]
+
+    for ch in roman_string:
+        for r in keys:
+            if romans.get(ch) <= last:
+                num += subza(idx)
+                idx = [romans.get(ch)]
+            else:
+                idx.append(romans.get(ch))
+            last = romans.get(ch)
+    num += subza(idx)
+
+    return num
