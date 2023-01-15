9591105295658c7f4d4e387f16ba225990d181ea
brian-ikiara <brnikiara@gmail.com>
2023-01-15 11:05:40 +0000

    Update 12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-def subza(idx):
-    res = 0
-    max_idx = max(idx)
+def to_subtract(list_num):
+    to_sub = 0
+    max_list = max(list_num)
 
-    for i in idx:
-        if max_idx > i:
-            res += i
-    return (max_idx - res)
+    for n in list_num:
+        if max_list > n:
+            to_sub += n
+
+    return (max_list - to_sub)
 
 
 def roman_to_int(roman_string):
@@ -14,21 +15,25 @@ def roman_to_int(roman_string):
         return 0
     if not isinstance(roman_string, str):
         return 0
-    romans = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    keys = list(romans.keys())
+
+    rom_n = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    list_keys = list(rom_n.keys())
 
     num = 0
-    last = 0
-    idx = [0]
+    last_rom = 0
+    list_num = [0]
 
     for ch in roman_string:
-        for r in keys:
-            if romans.get(ch) <= last:
-                num += subza(idx)
-                idx = [romans.get(ch)]
-            else:
-                idx.append(romans.get(ch))
-            last = romans.get(ch)
-    num += subza(idx)
+        for r_num in list_keys:
+            if r_num == ch:
+                if rom_n.get(ch) <= last_rom:
+                    num += to_subtract(list_num)
+                    list_num = [rom_n.get(ch)]
+                else:
+                    list_num.append(rom_n.get(ch))
 
-    return num
+                last_rom = rom_n.get(ch)
+
+    num += to_subtract(list_num)
+
+    return (num)
