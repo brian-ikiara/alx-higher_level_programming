06a8e4992ccc76370e0037c4681571d1c9b6cd80
brian-ikiara <brnikiara@gmail.com>
2023-01-23 14:33:13 +0000

    Update 0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,9 +2,8 @@
 def safe_print_list(my_list=[], x=0):
     i = 0
     try:
-        for i in my_list[:x]:
-            print("{:d}".format(my_list[i]), end="")
-            i += 1
+        for i in range(x):
+            print("{:d}".format(my_list[i]), end='')
     except IndexError:
         print("Exceeds list's bounds")
     print()
