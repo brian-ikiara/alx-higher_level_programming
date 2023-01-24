9c22dc582e344659850ef4bc2984d28bac0f5d1b
brian-ikiara <brnikiara@gmail.com>
2023-01-24 11:31:41 +0000

    Create 2-square.py

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Defines a square."""
+
+
+class Square:
+    """Represents a square."""
+
+    def __init__(self, size=0):
+        """Instantiate private attribute size.
+
+        Args:
+            size (int): The size of the square.
+
+        """
+        if isinstance(size, int):
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            self.__size = size
+        else:
+            raise TypeError("size must be an integer")
