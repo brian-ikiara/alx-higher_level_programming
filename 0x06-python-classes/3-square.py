ab396c3b194db492110906e88e9fdb6dae15935e
brian-ikiara <brnikiara@gmail.com>
2023-01-24 11:47:50 +0000

    Create 3-square.py

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""Defines a square."""
+
+
+class Square:
+    """Represents a square.
+
+    Demonstrates the various operations that can be performed on
+    a square such as Area.
+
+    """
+
+    def __init__(self, size=0):
+        """Instantiate private attribute size.
+        
+        Args:
+            size (int): The size of the square.
+
+        """
+        if isinstance(size, int):
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            self.__size = size
+        else:
+            raise TypeError("size must be an integer")
+
+
+    def area(self):
+        """Calculate the square's area.
+
+        Returns:
+            a (int): The square's area.
+
+        """
+        a = self.__size ** 2
+        return a
