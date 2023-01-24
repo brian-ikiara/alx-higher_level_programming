9595bea951bd7ccac7bd0af245dafb4c22a81bca
brian-ikiara <brnikiara@gmail.com>
2023-01-24 12:51:10 +0000

    Create 4-square.py

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""Defines a square."""
+
+
+class Square:
+    """Represents a square."""
+
+    def __init__(self, size=0):
+        """Instantiate the private attribute called size.
+
+        Args:
+            size (int): Square size.
+
+        """
+        if isinstance(size, int):
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            self.__size = size
+        else:
+            raise TypeError("size must be an integer")
+
+    @property
+    def size(self):
+        """Get the square's size.
+
+        Returns:
+            Size of the square.
+
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Set the square size.
+
+        Args:
+            value (int): Value to be set.
+
+        """
+        if isinstance(value, int):
+            if value < 0:
+                raise ValueError("size must be >= 0")
+            self.__size = value
+        else:
+            raise TypeError("size must be an integer")
+
+    def area(self):
+        """Calculate the square's area.
+
+        Returns:
+            The square's area.
+
+        """
+        return self.__size ** 2
