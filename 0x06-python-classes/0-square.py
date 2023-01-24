f1afaf50e2251530efcf43266d414e4883ddc46f
brian-ikiara <brnikiara@gmail.com>
2023-01-24 10:09:21 +0000

    Update 0-square.py

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,25 @@
 #!/usr/bin/python3
+"""Defines a square.
+
+Example:
+    Will be used as follows::
+        >>> Square = __import__('0-square').Square
+        >>> my_square = Square()
+        >>> print(type(my_square))
+        <class '0-square.Square'>
+        >>> print(my_square.__dict__)
+        {}
+
+"""
+
 class Square:
-    """Defines a square."""
+    """Represents a square.
+
+    Args:
+        none
+
+    Attributes:
+        none
+
+    """
     pass
