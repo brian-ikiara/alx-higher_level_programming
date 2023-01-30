91493f6914a27f6e108614afc515402373405414
brian-ikiara <brnikiara@gmail.com>
2023-01-30 19:40:12 +0000

    Update 3-rectangle.py

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -101,9 +101,11 @@ class Rectangle:
 
         """
         if self.__width > 0 and self.__height > 0:
+            rect = []
             for i in range(self.__height):
-                for j in range(self.__width):
-                    print("#", end='')
-                print()
+                [rect.append('#') for j in range(self.__width)]
+                if i != self.__height - 1:
+                    rect.append("\n")
+            return ("".join(rect))
         else:
             return str()
