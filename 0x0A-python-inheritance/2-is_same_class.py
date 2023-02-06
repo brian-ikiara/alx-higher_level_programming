cb832f6a28374b4aa6a79ca0728f170365c8a12a
brian-ikiara <brnikiara@gmail.com>
2023-02-06 18:18:06 +0000

    Create 2-is_same_class.py

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Implementation of isinstance().
+
+This will be interesting.
+
+"""
+
+
+def is_same_class(obj, a_class):
+    """Check if object is an instance of a class.
+
+    Args:
+        obj ('obj':'any'): The object
+        a_class ('obj':'class'): The class
+
+    Returns:
+        True: isinstance(obj, a_class)
+        False: !isinstance(obj, a_class)
+
+    """
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
