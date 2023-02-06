59ec6035a2d722418d8a3633c894c417f4c0bb06
brian-ikiara <brnikiara@gmail.com>
2023-02-06 18:21:19 +0000

    Update 2-is_same_class.py

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -18,7 +18,7 @@ def is_same_class(obj, a_class):
         False: !isinstance(obj, a_class)
 
     """
-    if isinstance(obj, a_class):
+    if isinstance(obj, a_class) or issubclass(obj, a_class):
         return True
     else:
         return False
