61214ff088ace85af4dee5d6ae0afae34fe9271f
brian-ikiara <brnikiara@gmail.com>
2023-02-06 18:38:51 +0000

    Update 4-inherits_from.py

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -14,4 +14,6 @@ def inherits_from(obj, a_class):
         False: !issubclass(obj, a_class)
 
     """
-    return issubclass(obj, a_class)
+    if type(obj) is a_class:
+        return False
+    return isinstance(obj, a_class)
