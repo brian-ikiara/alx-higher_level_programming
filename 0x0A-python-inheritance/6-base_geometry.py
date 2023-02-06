fa85c280541aff02adb18e0255a053b4468de670
brian-ikiara <brnikiara@gmail.com>
2023-02-06 18:54:04 +0000

    Create  6-base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Experimenting with Geometry."""
+
+
+class BaseGeometry:
+    """Basic Geometry."""
+
+    def area(self):
+        """Raise an error due to non-implementation."""
+        raise Exception("area() is not implemented")
