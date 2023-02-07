85227e6c7b477f7716c0239dc5d11374bb8259de
brian-ikiara <brnikiara@gmail.com>
2023-02-07 12:56:22 +0000

    Create .gitignore, 0-read_file.py

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Intro to I/OOps in Python."""
+
+
+def read_file(filename=""):
+    """Read a file.
+
+    Args:
+        filename ('obj':'file':'UTF-8'): File
+
+    """
+    with open(filename, "r", encoding="UTF-8") as myFile:
+        myFile.read()
