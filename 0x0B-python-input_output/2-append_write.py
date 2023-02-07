80a04b8ba8f97185f87fa15dadf2471a7b1a0d3e
brian-ikiara <brnikiara@gmail.com>
2023-02-07 13:41:19 +0000

    Create 2-append_write.py

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Intro to I/O Ops in Python."""
+
+
+def append_write(filename="", text=""):
+    """Append data to a file.
+
+    Args:
+        filename ('obj':'file':'UTF-8'): File
+        text (str): Data
+
+    Returns:
+        The appended file and number of characters added.
+
+    """
+    with open(filename, 'a', encoding='UTF-8') as myFile:
+        return myFile.write(text)
+
+append_write('3-my_file.txt', 'Butter Dog')
