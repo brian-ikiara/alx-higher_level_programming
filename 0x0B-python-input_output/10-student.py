527562c836b48bbdeb7fc6667feba0d4b148511f
brian-ikiara <brnikiara@gmail.com>
2023-02-07 19:21:28 +0000

    Create 10-student.py

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""Just have some class."""
+
+
+class Student:
+    """Defines a student."""
+
+    def __init__(self, first_name, last_name, age):
+        """Instantiate the attributes.
+
+        Args:
+            first_name (str): Student's first name
+            last_name (str): Student's last name
+
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Lexiconally represent Student.
+
+        Args:
+            attrs ('obj':'any'): Student's instance attribute
+
+        Returns:
+            Dictionary representation of Student.
+
+        """
+        dictStudent = {}
+        if attrs is not None:
+            for k, v in self.__dict__.items():
+                if k in attrs:
+                    dictStudent[k] = v
+            return dictStudent
+        else:
+            return self.__dict__
